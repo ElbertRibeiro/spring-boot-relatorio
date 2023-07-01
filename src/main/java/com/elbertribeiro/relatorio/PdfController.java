83b208f231aad48f11534bdd6b07e480695cad54
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-06-30 22:22:22 -0300

    primeiro commit

--- src/main/java/com/elbertribeiro/relatorio/PdfController.java
+++ src/main/java/com/elbertribeiro/relatorio/PdfController.java
@@ -1,9 +1,6 @@
 package com.elbertribeiro.relatorio;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -11,33 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 @RestController
 public class PdfController {
+    @Autowired
+    private PdfServico pdfServico;
 
     @GetMapping(value = "/report", produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<InputStreamResource> generatePdfReport() throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-        try (PDDocument document = new PDDocument()) {
-            PDPage page = new PDPage();
-            document.addPage(page);
-
-            PDPageContentStream contentStream = new PDPageContentStream(document, page);
-            contentStream.beginText();
-            contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
-            contentStream.newLineAtOffset(100, 700);
-            contentStream.showText("Exemplo de relatório em PDF");
-            contentStream.endText();
-            contentStream.close();
-
-            document.save(baos);
-        }
-
-        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
 
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=relatorio.pdf");
@@ -46,6 +26,6 @@ public class PdfController {
                 .ok()
                 .headers(headers)
                 .contentType(MediaType.APPLICATION_PDF)
-                .body(new InputStreamResource(bais));
+                .body(pdfServico.criandoPdfV1());
     }
 }
--- src/main/java/com/elbertribeiro/relatorio/PdfServico.java
+++ src/main/java/com/elbertribeiro/relatorio/PdfServico.java
@@ -1,10 +1,37 @@
 package com.elbertribeiro.relatorio;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @Service
 public class PdfServico {
-    public void teste(){
+    public InputStreamResource criandoPdfV1() throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
+        try (PDDocument document = new PDDocument()) {
+            PDPage page = new PDPage();
+            document.addPage(page);
+
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+            contentStream.beginText();
+            contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+            contentStream.newLineAtOffset(100, 700);
+            contentStream.showText("Exemplo de relatório em PDF");
+            contentStream.endText();
+            contentStream.close();
+
+            document.save(baos);
+        }
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        return new InputStreamResource(bais);
     }
 }
