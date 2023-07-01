042caaf94c2f7009c7614c8cfbc5d9e60b4a7b5e
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-06-30 22:40:15 -0300

    pdf com imagem

--- src/main/java/com/elbertribeiro/relatorio/PdfController.java
+++ src/main/java/com/elbertribeiro/relatorio/PdfController.java
@@ -1,6 +1,13 @@
 package com.elbertribeiro.relatorio;
 
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -8,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 @RestController
@@ -24,5 +33,33 @@ public class PdfController {
                 .headers(headers)
                 .contentType(MediaType.APPLICATION_PDF)
                 .body(pdfServico.criandoPdfV1());
+    }
+
+    @GetMapping(value = "/report/v2", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> generatePdfReportV2() throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (PDDocument document = new PDDocument()) {
+            PDPage page = new PDPage(PDRectangle.A4);
+            document.addPage(page);
+
+            PDImageXObject image = PDImageXObject.createFromFileByContent(
+                    new ClassPathResource("images/imagem.jpeg").getFile(), document);
+
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+            contentStream.drawImage(image, 100, 700, (float) image.getWidth() / 2, (float) image.getHeight() / 2);
+            contentStream.close();
+            document.save(baos);
+        }
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=relatorio.pdf");
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(new InputStreamResource(bais));
     }
 }
